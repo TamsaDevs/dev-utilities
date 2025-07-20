219d5b4f6f48ec8d4bd3ba59500a15c6981417d9
Brajmohan <brajmohan@indiamart.com>
2025-07-20 17:07:40 +0530

    feat: implement JSON Compare page with metadata setup

---
+++ src/app/json-compare/page.tsx
@@ -0,0 +1,10 @@
+import { metadata } from "../layout";
+
+export default function JSONComparePage() {
+  
+  metadata.title = "JSON Compare";
+  metadata.description = "Compare and analyze JSON data side by side with our online JSON compare tool. Easily spot differences and similarities in your JSON structures.";
+  return (
+    <h1>Coming Soon</h1>
+  );
+}
